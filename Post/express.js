36e142f0bbb1d986264561a331562eb7c8174261
Jethro-Canlas <jethrocanlas7@gmail.com>
2025-08-18 11:31:44 +0800

    File Upload

--- Post/express.js
+++ Post/express.js
@@ -1,7 +1,22 @@
 import express, { response } from 'express';
+import multer from 'multer';
 import bodyParser from 'body-parser';
+import { stripVTControlCharacters } from 'node:util';
+import { fileURLToPath } from 'node:url';
 const __dirname = import.meta.dirname;
 const app = express();
+
+var storage = multer.diskStorage({
+    destination: (req, file, callback ) => {
+        callback(null, 'uploads/');
+    },
+    filename: (req, file, callback) => {
+        callback(null, file.originalname);
+    }
+})
+
+var upload = multer({storage: storage}).fields([{name: 'file', maxCount: 1}]);
+
 const urlEncoderParser = bodyParser.urlencoded({extended:false});
 app.use(express.static('public'));
 // ROUTES
@@ -17,6 +32,10 @@ app.get('/studentPage', (req, res) => {
 app.get('/adminPage', (req, res) => {
     res.sendFile(__dirname + '/pages/admin.html')
 });
+app.get('/fileUpload', (req, res) => {
+    res.sendFile(__dirname + '/pages/fileUpload.html')
+});
+
 
 // API
 app.get('/getStudent', (req, res) => {
@@ -38,6 +57,23 @@ app.post('/postAdmin', urlEncoderParser, (req, res) => {
     }
     console.log("Respones is: ", response);
     res.end(`Received Data: ${JSON.stringify(response)}`)
+})
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) return res.statusCode(400).send('Error Uploading File');
+        
+        var response = {
+        studentId: req.body.adminId,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        department: req.body.department
+    }
+        const uploadedFile = req.files['file'][0];
+
+        console.log(`File path: ${uploadedFile.path}`)
+
+        res.end(`File and form data uploaded successfully`);
+    })
 })
 const server = app.listen(5000, () => {
     const host = server.address().address;
